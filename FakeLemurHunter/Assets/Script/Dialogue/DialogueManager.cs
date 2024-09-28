47bbea18fda16a15622596746ed63ad9140182e9
Dominika Brzozowska <dombrz09@wp.pl>
2024-09-29 00:24:23 +0200

    add load json

---
+++ FakeLemurHunter/Assets/Script/Dialogue/DialogueManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.IO;
+using Assets.Script.Dialogue.Enums;
+using System.Linq;
+
+namespace Assets.Script.Dialogue
+{
+
+    public class DialogueManager : MonoBehaviour
+    {
+        private Dialogue dialogue;
+
+        void Start()
+        {
+            string jsonFilePath = Path.Combine(Application.streamingAssetsPath, "dialogue.json");
+            string json = File.ReadAllText(jsonFilePath);
+            dialogue = JsonUtility.FromJson<Dialogue>(json);
+
+            var questionsDictionary = dialogue.GetQuestionsDictionary();
+
+            //if (questionsDictionary.TryGetValue("SCENARIO_01_KITCHEN_01", out var scenarioQuestions))
+            //{
+            //    foreach (var question in scenarioQuestions)
+            //    {
+            //        Debug.Log($"Question: {question.QuestionContent}, Answer: {question.Answer}, UnityObject: {question.UnityObject}");
+            //    }
+            //}
+        }
+    }
+}
